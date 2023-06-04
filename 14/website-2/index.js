0dff1568a90f4bd880b1df218f0e0ce670c80ace
tzzle <130739462+tzzle@users.noreply.github.com>
2023-06-04 15:08:48 +0300

    Add files via upload

---
+++ 14/website-2/index.js
@@ -0,0 +1,8 @@
+function disappear() {
+    document.getElementById('hide').style.display = 'block';
+    document.getElementById('hideButton').style.display = 'none';
+}
+
+function change() {
+    document.getElementById('showButton').style.display = 'none';
+}
